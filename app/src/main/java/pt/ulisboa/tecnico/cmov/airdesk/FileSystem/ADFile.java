b1e10d030c8c13ab11afb0e87a638fe5eb9f2c47
Filipe Teixeira <filipelteixeira@gmail.com>
2015-03-12 12:07:26 +0000

    File handling
    Added some functions to ADFile behaviour, exceptions still need handling

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/FileSystem/ADFile.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/FileSystem/ADFile.java
@@ -1,30 +1,45 @@
 package pt.ulisboa.tecnico.cmov.airdesk.FileSystem;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import pt.ulisboa.tecnico.cmov.airdesk.Workspace.Workspace;
 
 /**
  * Created by hugo__000 on 12/03/2015.
  */
 public class ADFile {
 
-    File file;
+    private File file;
+    private boolean editable;
+    private Workspace workspace;
 
-    public ADFile(String name) {
-        file = new File(name);
+    public ADFile(String name, Workspace workspace) {
+        file = new File(name + ".txt");
+        editable = true;
+        this.workspace = workspace;
     }
 
     public File getFile() {
         return file;
     }
 
-    public boolean isBeingEdited() {
-        return false;
+    public boolean isEditable() {
+        return editable;
     }
 
-    public void edit(){
-
-    }
-
-    public void save(){
-
+    public void save(String text){
+        //TODO: Exception handle
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(file.getName(), "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        writer.println(text);
+        writer.close();
     }
 }
